6e26740d8ac48d335ae3e9fd21df311a31854b58
martinsdad <48945072+martinsdad@users.noreply.github.com>
2019-03-28 17:48:02 +0300

    ref exp new user

--- other/express_new_user.js
+++ other/express_new_user.js
@@ -1,33 +1,38 @@
+let login = ':nth-child(1) > .form-control'// поле логин
+let password = ':nth-child(2) > .form-control' // поле пароль
+let basket = '._3L1Qv > ._2-2Tl' // корзина в шапке
+let addBasket = ':nth-child(1) > :nth-child(13) > ._1rCPk > .nowrap > ._3Iwt4' //Положить в корзину
+let callButton = ':nth-child(1) > :nth-child(13) > ._1rCPk > .nowrap > ._3ZaEA > ._2-2Tl' //кнопка звонка
+let fieldPhone = '._1sdN5 > div > .form-control' // поле телефон
+let fieldCode = '#order-code' // поле кода подтверждения
+let profile = '._2JR3u' // личный кабинет(мыло)
+
 describe('Экспресс новый юзер', function() {
   beforeEach('cookie', function() {
-  cy.server()
-      cy.route('GET', 'https://api-express.apgrup.ru/app_dev.php/v2/partnames/existing')
-          .as('ww')
-      cy.route('GET', 'https://api-express.apgrup.ru/app_dev.php/v1/checkspam/sms?type=alfadetali')
-          .as('ee')
-})
+   cy.server()
+      cy.route('GET', 'https://api-express.apgrup.ru/app_dev.php/v2/partnames/existing').as('ww');
+      cy.route('GET', 'https://api-express.apgrup.ru/app_dev.php/v1/checkspam/sms?type=alfadetali').as('ee');
+});
 
-      it('visit', function() {
+    it('visit', function() {
       cy.clearCookies()
       cy.visit('https://express.apgrup.ru');
-})
+});
 
 //Авторизация
-   it('auth', function() {
-      cy.get(':nth-child(1) > .form-control')
-      .type('nokenivune@datasoma.com').should('value', 'nokenivune@datasoma.com');
-      cy.get(':nth-child(2) > .form-control').click().type('123456');
+    it('auth', function() {
+      cy.get(login).type('nokenivune@datasoma.com').should('value', 'nokenivune@datasoma.com');
+      cy.get(password).click().type('123456');
       cy.get('._3REB8').contains('Войти').click();
-})
+});
 
 //Цена город компания
-   it('gorod and company', function() {
+    it('gorod and company', function() {
       cy.get('._2JR3u').contains('0').should('be.visible');
       cy.get('._NY4w').contains(',00').should('not.be.visible');
       cy.get('._2rfW9').contains('Бампер задний').click();
       cy.get('._15-2L').contains('Компания').should('not.be.visible');
       cy.get('._15-2L').contains('Город').should('not.be.visible');
       cy.get('._15-2L').contains('Показать телефон').should('not.be.visible');
-})
-
-})
+});
+});
