9627e3ce38adfe5d47f5dd7177c271dbe6692778
martinsdad <48945072+martinsdad@users.noreply.github.com>
2019-03-28 17:36:02 +0300

    refactoring express_sto

--- other/express_sto.js
+++ other/express_sto.js
@@ -1,126 +1,72 @@
+let login = ':nth-child(1) > .form-control'// поле логин
+let password = ':nth-child(2) > .form-control' // поле пароль
+let basket = '._3L1Qv > ._2-2Tl' // корзина в шапке
+let addBasket = ':nth-child(1) > :nth-child(13) > ._1rCPk > .nowrap > ._3Iwt4' //Положить в корзину
+let callButton = ':nth-child(1) > :nth-child(13) > ._1rCPk > .nowrap > ._3ZaEA > ._2-2Tl' //кнопка звонка
+let fieldPhone = '._1sdN5 > div > .form-control' // поле телефон
+let fieldCode = '#order-code' // поле кода подтверждения
+let profile = '._2JR3u' // личный кабинет(мыло)
+
 describe('Экспресс СТО', function() {
-  beforeEach('cookie', function() {
-  cy.server()
-      cy.route('GET', 'https://api-express.apgrup.ru/app_dev.php/v2/partnames/existing')
-          .as('ww')
-      cy.route('GET', 'https://api-express.apgrup.ru/app_dev.php/v1/checkspam/sms?type=alfadetali')
-          .as('ee')
+   beforeEach('cookie', function() {
+       cy.server()
+       cy.route('GET', 'https://api-express.apgrup.ru/app_dev.php/v2/partnames/existing').as('ww');
+       cy.route('GET', 'https://api-express.apgrup.ru/app_dev.php/v1/checkspam/sms?type=alfadetali').as('ee');
 })
 
-      it('visit', function() {
-      cy.clearCookies()
-      cy.visit('https://express.apgrup.ru')
+   it('visit', function() {
+       cy.clearCookies()
+       cy.visit('https://express.apgrup.ru')
 })
 
 //Авторизация
    it('auth', function() {
-      cy.get('#app > div._wrapper_owfcp_1 > div > form > div:nth-child(1) > input')
-          .click()
-          .type('menufolesu@2mailnext.com')
-          .should('value', 'menufolesu@2mailnext.com')
-      cy.get('#app > div._wrapper_owfcp_1 > div > form > div:nth-child(2) > input')
-          .click()
-          .type('123456')
-      cy.get('#app > div._wrapper_owfcp_1 > div > form > p > button')
-          .click()
+       cy.get(login).type('menufolesu@2mailnext.com').should('value', 'menufolesu@2mailnext.com');
+       cy.get(password).type('123456');
+       cy.get('._3REB8').contains('Войти').click();
 })
 
 //Город и компания
    it('gorod and company', function() {
-      cy.get('._container_1k0mk_7')
-          .contains(',00')
-          .should('be.visible')
-      cy.get(':nth-child(1) > [style="width: 164px;"] > .link')
-          .click( {force:true} )
-      cy.get('._modal_v4kgj_21')
-          .contains('Компания')
-          .should('be.visible')
-      cy.get('._modal_v4kgj_21')
-          .contains('Город')
-          .should('be.visible')
-      cy.get('._modal_v4kgj_21')
-          .contains('Показать телефон')
-          .should('be.visible')
-          .click()
-      cy.get('._modal_v4kgj_21')
-          .contains('Контактная информация')
-          .should('be.visible')
-      cy.get('.clear')
-          .click()
-      cy.get(':nth-child(1) > [style="width: 8rem;"] > ._callButton_909cd_25')
-          .click()
-      cy.get('._modal_v4kgj_21')
-          .contains('Контактная информация')
-          .should('be.visible')
-      cy.get('.clear')
-          .click()
+       cy.get('._2JR3u').contains('0').should('be.visible');
+       cy.get('._NY4w').contains(',00').should('be.visible');
+       cy.get('._2rfW9').contains('Бампер задний').click();
+       cy.get('._15-2L').contains('Компания').should('be.visible');
+       cy.get('._15-2L').contains('Город').should('be.visible');
+       cy.get('._15-2L').contains('Показать телефон').should('be.visible').click();
+       cy.get('._15-2L').contains('Контактная информация').should('be.visible');
+       cy.get('._3Bz1P').click();
+       cy.wait(3000);
 })
 
 //Оформить заказ
    it('add and order', function() {
-     cy.get(':nth-child(1) > [style="width: 8rem;"] > ._button_1dddp_1')//Положить в корзину
-         .click( {force: true} )
-     cy.get('#app > div._wrapper_1k0mk_1 > div > div._wrapper_j3yuq_1 > div > div._wrapper_1w9dx_1 > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(12) > button.btn.btn-success._callButton_909cd_25')//Кнопка звонка
-         .should('be.visible')
-     cy.get('._cart_lp3qm_1 > ._icon_saydd_1')//Корзина в шапке
-         .click( {waitForAnimations: false} )
-     cy.get('._container_1k0mk_7')
-         .contains('товары на сумму')
-         .should('be.visible')
-        cy.wait(3000)
-     cy.get('._container_1k0mk_7')
-         .contains('Оформить заказ')
-         .click( {waitForAnimations: false} )
-     cy.wait('@ee')
-     cy.wait(3000)
-     cy.get('._code_1frsw_25 > div > .form-control')//Поле телефон
-         .click()
-         .type('1234567891')
-         .should('value', '+7 (123) 456-78-91')
-     cy.get('body')
-         .contains('Отправить смс с кодом подтверждения')
-         .click()
-     cy.get('#order-code')
-         .click()
-         .type('123456')
-     cy.get('body')
-         .contains('Отправить код подтверждения')
-         .click()
-     cy.get('body')
-         .contains('Заказ оформлен, спасибо!')
-         .should('be.visible')
+       cy.get(addBasket).click( {force: true} );
+       cy.get(callButton).should('be.visible');
+       cy.get(basket).click( {force:true} );
+       cy.get('._3Szvf').contains('Оформить заказ').click();
+       cy.wait('@ee');
+       cy.wait(3000);
+       cy.get(fieldPhone).type('1234567891').should('value', '+7 (123) 456-78-91');
+       cy.get('body').contains('Отправить смс с кодом подтверждения').click();
+       cy.get(fieldCode).type('123456');
+       cy.get('body').contains('Отправить код подтверждения').click();
+       cy.get('body').contains('Заказ оформлен, спасибо!').should('be.visible');
 })
 
 //История заказов
    it('history', function() {
-     cy.get('._wrapper_1k0mk_1')
-         .contains('menufolesu@2mailnext.com')
-         .click()
-     cy.get('._modal_v4kgj_21')
-         .contains('История заказов')
-         .click()
-     cy.get('._container_1k0mk_7')
-         .contains('Новый')
-         .should('be.visible')
+       cy.get(profile).contains('menufolesu@2mailnext.com').click();
+       cy.get('._15-2L').contains('История заказов').click();
+     //cy.get('._NY4w').contains('Новый').should('be.visible');
 })
 
 //Смена пароля
    it('change password', function() {
-     cy.get('._wrapper_1k0mk_1')
-         .contains('menufolesu@2mailnext.com')
-         .click()
-     cy.get('[placeholder="Новый пароль (не менее 6 символов)"]')
-         .click()
-         .type('123456')
-     cy.get('[placeholder="Повтор нового пароля"]')
-         .click()
-         .type('123456')
-     cy.get('._modal_v4kgj_21')
-         .contains('Сохранить')
-         .click()
-     cy.get('._modal_v4kgj_21')
-         .contains('Пароль успешно изменен')
-         .should('be.visible')
-})
-
-})
+       cy.get(profile).contains('menufolesu@2mailnext.com').click();
+       cy.get('[placeholder="Новый пароль (не менее 6 символов)"]').click().type('123456');
+       cy.get('[placeholder="Повтор нового пароля"]').click().type('123456');
+       cy.get('._15-2L').contains('Сохранить').click();
+       cy.get('._15-2L').contains('Пароль успешно изменен').should('be.visible');
+});
+});
