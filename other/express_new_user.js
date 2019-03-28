f13895876a17a87cd1cccbb8a17b2dfb597b8021
martinsdad <48945072+martinsdad@users.noreply.github.com>
2019-03-28 15:53:14 +0300

    refactoring express_new_user

--- other/express_new_user.js
+++ other/express_new_user.js
@@ -9,45 +9,25 @@ describe('Экспресс новый юзер', function() {
 
       it('visit', function() {
       cy.clearCookies()
-      cy.visit('https://express.apgrup.ru')
+      cy.visit('https://express.apgrup.ru');
 })
 
 //Авторизация
    it('auth', function() {
-      cy.get('#app > div._wrapper_owfcp_1 > div > form > div:nth-child(1) > input')
-          .click()
-          .type('nokenivune@datasoma.com')
-          .should('value', 'nokenivune@datasoma.com')
-      cy.get('#app > div._wrapper_owfcp_1 > div > form > div:nth-child(2) > input')
-          .click()
-          .type('123456')
-      cy.get('#app > div._wrapper_owfcp_1 > div > form > p > button')
-          .click()
+      cy.get(':nth-child(1) > .form-control')
+      .type('nokenivune@datasoma.com').should('value', 'nokenivune@datasoma.com');
+      cy.get(':nth-child(2) > .form-control').click().type('123456');
+      cy.get('._3REB8').contains('Войти').click();
 })
 
 //Цена город компания
    it('gorod and company', function() {
-      cy.get(':nth-child(1) > [style="width: 8rem;"] > div > .btn')
-          .click( {force:true} )
-      cy.get('._total_lp3qm_11')
-          .contains('0')
-          .should('be.visible')
-      cy.get('._container_1k0mk_7 > :nth-child(3)')
-          .contains(',00')
-          .should('not.be.visible')
-      cy.get(':nth-child(1) > [style="width: 164px;"] > .link')
-          .click( {force:true} )
-      cy.get('._modal_v4kgj_21')
-          .contains('Компания')
-          .should('not.be.visible')
-      cy.get('._modal_v4kgj_21')
-          .contains('Город')
-          .should('not.be.visible')
-      cy.get('._modal_v4kgj_21')
-          .contains('Показать телефон')
-          .should('not.be.visible')
-      cy.get('.clear')
-          .click()
+      cy.get('._2JR3u').contains('0').should('be.visible');
+      cy.get('._NY4w').contains(',00').should('not.be.visible');
+      cy.get('._2rfW9').contains('Бампер задний').click();
+      cy.get('._15-2L').contains('Компания').should('not.be.visible');
+      cy.get('._15-2L').contains('Город').should('not.be.visible');
+      cy.get('._15-2L').contains('Показать телефон').should('not.be.visible');
 })
 
 })
