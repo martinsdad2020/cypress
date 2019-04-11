6b9081f0a380c2d22a0f2ce8efed379002c19207
martinsdad <48945072+martinsdad@users.noreply.github.com>
2019-04-11 12:51:07 +0300

    fixed

--- other/express_sto.js
+++ other/express_sto.js
@@ -1,8 +1,8 @@
 let login = ':nth-child(1) > .form-control'// поле логин
 let password = ':nth-child(2) > .form-control' // поле пароль
 let basket = '._3L1Qv > ._2-2Tl' // корзина в шапке
-let addBasket = ':nth-child(1) > :nth-child(13) > ._1rCPk > .nowrap > ._3Iwt4 > ._2-2Tl' //Положить в корзину
-let callButton = ':nth-child(1) > :nth-child(13) > ._1rCPk > .nowrap > ._3ZaEA > ._2-2Tl' //кнопка звонка
+let addBasket = ':nth-child(1) > :nth-child(12) > ._1rCPk > .nowrap > ._3Iwt4' //Положить в корзину
+let callButton = ':nth-child(1) > :nth-child(12) > ._1rCPk > .nowrap > ._3ZaEA' //кнопка звонка
 let profile = '._2JR3u' // личный кабинет(мыло)
 
 describe('express sto', function() {
@@ -18,7 +18,7 @@ describe('express sto', function() {
 });
 
    it('auth', function() {
-       cy.get(login).type('menufolesu@2mailnext.com').should('value', 'menufolesu@2mailnext.com');
+       cy.get(login).type('menufolesu_prod@2mailnext.com').should('value', 'menufolesu_prod@2mailnext.com');
        cy.get(password).type('123456');
        cy.get('._3REB8').contains('Войти').click();
 });
@@ -32,7 +32,7 @@ describe('express sto', function() {
        cy.get('._15-2L').contains('Показать телефон').should('be.visible').click();
        cy.get('._15-2L').contains('Контактная информация').should('be.visible');
        cy.get('._3Bz1P').click();
-       cy.wait(3000);
+       // cy.wait(3000);
 });
 
    it('add and order', function() {
@@ -40,19 +40,19 @@ describe('express sto', function() {
        cy.get(callButton).should('be.visible');
        cy.get(basket).click();
        cy.get('._3Szvf').contains('Оформить заказ').click();
-       cy.wait(3000);
+       // cy.wait(3000);
        cy.get('._3Szvf').contains('Подтвердить').click();
        cy.get('body').contains('Заказ оформлен, спасибо!').should('be.visible');
 });
 
    it('history', function() {
-       cy.get(profile).contains('menufolesu@2mailnext.com').click();
+       cy.get(profile).contains('menufolesu_prod@2mailnext.com').click();
        cy.get('._15-2L').contains('История заказов').click();
        cy.get('._NY4w').contains('Новый').should('be.visible');
 });
 
    it('change password', function() {
-       cy.get(profile).contains('menufolesu@2mailnext.com').click();
+       cy.get(profile).contains('menufolesu_prod@2mailnext.com').click();
        cy.get('[placeholder="Новый пароль (не менее 6 символов)"]').click().type('123456');
        cy.get('[placeholder="Повтор нового пароля"]').click().type('123456');
        cy.get('._15-2L').contains('Сохранить').click();
