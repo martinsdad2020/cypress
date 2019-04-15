986c3d4f818195b4acbef3743731bc6b4addbbca
martinsdad <48945072+martinsdad@users.noreply.github.com>
2019-04-15 12:21:01 +0300

    new prod script

---
+++ prod/print_rashod_prod.js
@@ -0,0 +1,24 @@
+var mail = '#form-email'
+var pass = '#form-password'
+var enter = '.ng-valid-email.ng-valid-minlength > :nth-child(2) > .btn'
+
+describe("status", function() {
+   before('cookie', function() {
+      cy.setCookie('guid-1', '%7B153B24F9-C1FC-D813-A0B7-038BD34C6733%7D');
+   });
+
+   it("Auth", function() {
+      cy.visit("https://alfa-mt.ru/saleinvoices/46425/");
+
+      cy.get(mail).click().clear().type('ra-nt-office@yandex.ru');
+      cy.get(pass).click().clear().type('654321');
+      cy.get(enter).click();
+      cy.wait(2000);
+   });
+
+   it('Print', function() {
+   	  cy.get('.app__content').contains('Печать ТТН').click();
+   	  cy.get('.app__content').contains('Печать Товарного чека').click();
+   	  cy.get('.app__content').contains('Печать Торг 12').click();
+   });
+});
