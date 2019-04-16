4ed88b0c5c57587a7c22e01c0fe778d45267dbc9
martinsdad <48945072+martinsdad@users.noreply.github.com>
2019-04-16 11:46:48 +0300

    fix

--- other/magaz_mobile_version.js
+++ other/magaz_mobile_version.js
@@ -4,111 +4,108 @@
 describe('mobile version magaz', function() {
 
     it('visit', function() {
-    cy.clearCookies()
-    cy.visit('https://magaz.apgrup.ru/')
+      cy.clearCookies()
+      cy.visit('https://magaz.apgrup.ru/');
 
 
 //Переход на мобильную версию, убеждаемся что бургер отображается
       cy.get('._1rlq').contains('Мобильная версия')
-          .click()
+        .click();
       cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .should('be.visible')
+        .should('be.visible');
 
 
 //Идем в раздел личный кабинет и восстанавливаем пароль, проверка регистрации.
       cy.get (':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
       cy.get('._3w0n > :nth-child(1)')
-          .contains('Личный кабинет')
-          .click()
+        .contains('Личный кабинет')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Вход')
-          .should('be.visible')
+        .contains('Вход')
+        .should('be.visible');
       cy.get('[data-reactroot=""]')
-          .contains('Войти')
-          .click()
+        .contains('Войти')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Введите эл. почту')
-          .should('be.visible')
+        .contains('Введите эл. почту')
+        .should('be.visible');
       cy.get('[data-reactroot=""]')
-          .contains('Введите пароль')
-          .should('be.visible')
+        .contains('Введите пароль')
+        .should('be.visible');
       cy.get('[data-reactroot=""]')
-          .contains('Забыли пароль?')
-          .click()
+        .contains('Забыли пароль?')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Восстановление пароля')
-          .should('be.visible')
+        .contains('Восстановление пароля')
+        .should('be.visible');
       cy.get('.wQHd > .cNzu > ._2l3F')
-          .click()
-          .type('xikefa@cliptik.net')
-    /*cy.get('form > ._3Zzt')
-          .contains('Восстановить')
-          .click() // КАПЧА!!!!
+        .click()
+        .type('wrong_parole@provider.com');
+    cy.get('form > ._3Zzt')
+        .contains('Восстановить')
+        .click();
       cy.get('html')
-          .contains('На ваш почтовый ящик было отправлено письмо')
-          .should('be.visible')*/
-
+        .contains('На ваш почтовый ящик было отправлено письмо')
+        .should('be.visible');
 
 //Регистрация
       cy.get (':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
       cy.get('._3w0n > :nth-child(1)')
-          .contains('Личный кабинет')
-          .click()
+        .contains('Личный кабинет')
+        .click();
       cy.get('[href="/register"]')
-          .contains('Регистрация')
-          .click()
+        .contains('Регистрация')
+        .click();
       cy.get('._1VYP > :nth-child(1)')
-          .contains('Эл.почта')
-          .should('be.visible')
+        .contains('Эл.почта')
+        .should('be.visible');
       cy.get('form > :nth-child(1) > .cNzu > ._2l3F')
-          .click()
-          .type('gg@ya.ru')
-          .should('value', 'gg@ya.ru')
+        .type('gg@ya.ru')
+        .should('value', 'gg@ya.ru');
       cy.get('._1VYP > :nth-child(1)')
-          .contains('Пароль*')
-          .should('be.visible')
+        .contains('Пароль*')
+        .should('be.visible');
       cy.get(':nth-child(2) > .cNzu > ._2l3F')
-          .click()
-          .type('111111')
-          .should('value', '111111')
+        .type('111111')
+        .should('value', '111111');
       cy.get('form > ._3Zzt')
-          .click()
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Вы не согласились с пользовательским соглашением')
-          .should('be.visible')
+        .contains('Вы не согласились с пользовательским соглашением')
+        .should('be.visible');
     /*cy.get('._3b2P')
         .contains('checkbox')*/ // хз как поставить галочку в чекбокс
 
 
 //Гуляем по разделам
       cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-        .click()
+        .click();
       cy.get('._3w0n > :nth-child(1)')
-          .contains('Корзина')
-          .click()
+        .contains('Корзина')
+        .click();
       cy.get('html')
-          .contains('Корзина пуста')
-          .should('be.visible')
+        .contains('Корзина пуста')
+        .should('be.visible');
       cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-        .click()
+        .click();
       cy.get('[href="/check_order"]')
-          .contains('Что с моим заказом?')
-          .click()
-          cy.wait(2000)
+        .contains('Что с моим заказом?')
+        .click();
+      cy.wait(2000);
       cy.get('._1VYP > :nth-child(1)')
-          .contains('Введите номер заказа')
-          .should('be.visible')
+        .contains('Введите номер заказа')
+        .should('be.visible');
       cy.get('._3pmE > ._3Zzt')
-          .click()
+        .click();
       cy.get('.tT7l')
-          .contains('Введите номер заказа')
-          .should('be.visible')
+        .contains('Введите номер заказа')
+        .should('be.visible');
       cy.get('._2a4O > .cNzu > ._2l3F')
-          .click()
-          .type('1234567890')
-          .should('value', '1234567890')
+        .click()
+        .type('1234567890')
+        .should('value', '1234567890');
     /*cy.get('._3pmE > ._3Zzt')
         .click()//КАПЧА!!! при проверке статуса заказа
       cy.get('.tT7l')
@@ -118,105 +115,102 @@ describe('mobile version magaz', function() {
 
 // бургер-главная страница-все марки
       cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
       cy.get('._3w0n > :nth-child(1)')
-          .contains('Главная страница')
-          .click()
+        .contains('Главная страница')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Все марки')
-          .should('be.visible')
-          .click()
+        .contains('Все марки')
+        .should('be.visible')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('ZAZ')
-          .should('be.visible')
+        .contains('ZAZ')
+        .should('be.visible');
       cy.get('[data-reactroot=""]')
-          .contains('Грузовые')
-          .click()
+        .contains('Грузовые')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('MAN')
-          .should('be.visible')
-          .click()
+        .contains('MAN')
+        .should('be.visible')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Neoplan')
-          .should('be.visible')
+        .contains('Neoplan')
+        .should('be.visible');
       cy.get(':nth-child(1) > ._2WQY > span')
-          .contains('Главная')
-          .click()
+        .contains('Главная')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Легковые')
-          .click()
+        .contains('Легковые')
+        .click();
      cy.get('[data-reactroot=""]')
-          .contains('Audi')
-          .should('be.visible')
-          .click()
+        .contains('Audi')
+        .should('be.visible')
+        .click();
      cy.get('[data-reactroot=""]')
-          .contains('A3')
-          .should('be.visible')
+        .contains('A3')
+        .should('be.visible');
 
 
 // О компании
      cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
      cy.get('._3w0n > :nth-child(1)')
-          .contains('О компании')
-          .click()
+        .contains('О компании')
+        .click();
      cy.get('[data-reactroot=""]')
-          .contains('Мы рады приветствовать вас')
-
-
-// Доставка
-          .should('be.visible')
+        .contains('Мы рады приветствовать вас')
+        .should('be.visible');
      cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
      cy.get('._3w0n > :nth-child(1)')
-          .contains('Доставка')
-          .click()
+        .contains('Доставка')
+        .click();
      cy.get('[data-reactroot=""]')
-          .contains('AlfaDetali осуществляет доставку Товара:')
-          .should('be.visible')
+        .contains('AlfaDetali осуществляет доставку Товара:')
+        .should('be.visible');
 
 
 // Как купить?
       cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
       cy.get('._3w0n > :nth-child(1)')
-          .contains('Как купить?')
-          .click()
+        .contains('Как купить?')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Процедура покупки товара в интернет-магазине')
-          .should('be.visible')
+        .contains('Процедура покупки товара в интернет-магазине')
+        .should('be.visible');
 
 
 // Контактная информация
       cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
       cy.get('._3w0n > :nth-child(1)')
-          .contains('Контактная информация')
-          .click()
+        .contains('Контактная информация')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Адрес: Россия, 198095, Санкт-Петербург')
-          .should('be.visible')
+        .contains('Адрес: Россия, 198095, Санкт-Петербург')
+        .should('be.visible');
 
 
 // Возврат и гарантия
       cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
       cy.get('._3w0n > :nth-child(1)')
-          .contains('Возврат и гарантия')
-          .click()
+        .contains('Возврат и гарантия')
+        .click();
       cy.get('[data-reactroot=""]')
-          .contains('Правила возврата Товара')
-          .should('be.visible')
+        .contains('Правила возврата Товара')
+        .should('be.visible');
 
 
 // Переход на полную версию сайта
       cy.get(':nth-child(2) > ._1WjC > label > ._3Zzt > .tysP')
-          .click()
+        .click();
       cy.get('._3w0n > :nth-child(1)')
-          .contains('Полная версия сайта')
-          .click()
+        .contains('Полная версия сайта')
+        .click();
       cy.get('._1rlq')
-          .contains('Вся информация, представленная на сайте alfadetali.ru')
-          .should('be.visible')
+        .contains('Вся информация, представленная на сайте alfadetali.ru')
+        .should('be.visible');
     })
 })
