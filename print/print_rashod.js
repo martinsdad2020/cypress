c8f7e90ab442fe4bcde643d07677ea4d97b12ac6
martinsdad <48945072+martinsdad@users.noreply.github.com>
2019-04-22 14:51:36 +0300

    fix

--- print/print_rashod.js
+++ print/print_rashod.js
@@ -2,12 +2,12 @@ var mail = '#form-email'
 var pass = '#form-password'
 var enter = '.ng-scope.ng-valid-minlength > :nth-child(2) > .btn'
 
-describe("status", function() {
-   before('cookie', function() {
+describe("status", function () {
+   before('cookie', function () {
       cy.setCookie('guid-191', '%7B1A4781F3-8401-B3AD-A5E7-2594B38BAC26%7D');
    });
 
-   it("Auth", function() {
+   it("Auth", function () {
       cy.visit("https://apgrup.ru/saleinvoices/45863/");
 
       cy.get(mail).click().clear().type('jinda.project@gmail.com');
@@ -16,11 +16,11 @@ describe("status", function() {
       cy.wait(2000);
    });
 
-   it('Print', function() {
-   	  cy.get('.ng-binding.ng-scope > span.ng-scope > [ng-click="click(0)"]').contains('Печать ТТН').click();
-   	  cy.get('[ng-click="click(1)"]').contains('Печать Товарного чека').click();
-   	  cy.get('[ng-click="click(2)"]').contains('Печать Торг 12').click();
-   	  cy.get('[ng-click="click(3)"]').contains('Печать TTH Альфадетали').click();
-   	  cy.get('[ng-click="click(4)"]').contains('Печать Торг 12 Альфадетали').click();
+   it('Print', function () {
+      cy.get('.ng-binding.ng-scope > span.ng-scope > [ng-click="click(0)"]').contains('Печать ТТН').click();
+      cy.get('[ng-click="click(1)"]').contains('Печать Товарного чека').click();
+      cy.get('[ng-click="click(2)"]').contains('Печать Торг 12').click();
+      cy.get('[ng-click="click(3)"]').contains('Печать TTH Альфадетали').click();
+      cy.get('[ng-click="click(4)"]').contains('Печать Торг 12 Альфадетали').click();
    });
 });
