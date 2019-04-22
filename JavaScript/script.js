febd9f72a1ab04d64d32afa4610a186b2150ccc0
MD <martinsdad@icloud.com>
2019-04-22 19:18:11 +0300

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+
+const pi = 3.14;
+let second = 2;
+console.log(second);
+
+// alert('Hello World');
+// let answer = confirm('Are you here?');
+// console.log(answer);
+
+let answer = prompt('Do you have 18 years old?', 'Да');
+console.log(typeof(answer));
---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+let second = 2;
+console.log(second);
