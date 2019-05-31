fd6a8df8d44714438ed03b6286155f4149ffd25e
MD <martinsdad@icloud.com>
2019-05-31 11:11:28 +0300

    а

--- other/01.magaz_smoke_test.js
+++ other/01.magaz_smoke_test.js
@@ -1,5 +1,5 @@
 //for (let i = 0; i < 10; i++) {
-let head = '._3WKx > :nth-child(1)'
+let head = '._3WKxw > :nth-child(1)'
 
 describe("магаз", function () {
 
@@ -50,20 +50,20 @@ describe("магаз", function () {
         cy.get('#root')
             .contains('Вход')
             .should('be.visible');
-        cy.get(':nth-child(1) > ._2EEn > .aEtU')
+        cy.get(':nth-child(1) > ._2EEno > .aEtUW')
             .click()
             .clear()
             .type('invalid@u.ru')
             .should('value', 'invalid@u.ru');
-        cy.get(':nth-child(2) > ._2EEn > .aEtU')
+        cy.get(':nth-child(2) > ._2EEno > .aEtUW')
             .click()
             .clear()
             .type('123456')
             .should('value', '123456');
-        cy.get('form > .iFde')
+        cy.get('form > .iFdeK')
             .click();
         cy.wait(3000);
-        cy.get('._2201 > form')
+        cy.get('#root')
             .contains('Пользователь с такой почтой не найден')
             .should('be.visible')
             .click();
@@ -81,19 +81,21 @@ describe("магаз", function () {
             .clear()
             .type('123456')
             .should('value', '123456');
-        cy.get('form > .iFde')
+        cy.get('form > .iFdeK')
             .click();
-        cy.get('._3WKx > :nth-child(1) > :nth-child(2)')
+        cy.get('#root')
             .contains('Личный кабинет')
-            .should('be.visible');
-        cy.get('.v3qQ')
+            .should('be.visible')
+            .click();
+        cy.get('#root')
             .contains('Выход')
             .click({ force: true });
     });
 
     //Восстановление пароля
     it('forgot pass', function () {
-        cy.get(':nth-child(2) > ._12eX > ._2qfA')
+        cy.get('#root')
+            .contains('Вход')
             .click();
         cy.get('#root')
             .contains('Забыли пароль?')
@@ -101,11 +103,11 @@ describe("магаз", function () {
         cy.get('#root')
             .contains('Восстановление пароля')
             .should('be.visible');
-        cy.get('.aEtU')
+        cy.get('.aEtUW')
             .clear()
             .type('wrong_parole@provider.com')
             .should('value', 'wrong_parole@provider.com');
-        cy.get('form > .iFde')
+        cy.get('form > .iFdeK')
             .click();
         cy.wait(2000);
         cy.get('#root')
@@ -114,71 +116,71 @@ describe("магаз", function () {
     });
     //Регистрация(поля)
     it('registration', function () {
-        cy.get(head)
+        cy.get('#root')
             .contains('Регистрация')
-            .click({ force: true });
+            .click();
         cy.get('[href="/register"]')
             .click();
         cy.get('#root')
             .contains('Эл.почта*')
             .should('be.visible');
-        cy.get(':nth-child(1) > ._2EEn > .aEtU')
+        cy.get(':nth-child(1) > ._2EEno > .aEtUW')
             .clear()
             .type('invalid@u.ru')
             .should('value', 'invalid@u.ru');
         cy.get('#root')
             .contains('Пароль*')
             .should('be.visible');
-        cy.get(':nth-child(2) > ._2EEn > .aEtU')
+        cy.get(':nth-child(2) > ._2EEno > .aEtUW')
             .clear()
             .type('123456')
             .should('value', '123456');
-        cy.get('form > .iFde')
+        cy.get('form > .iFdeK')
             .click();
-        cy.get('._3yLY')
+        cy.get('#root')
             .contains('Вы не согласились с пользовательским соглашением ')
             .should('be.visible');
     });
 
     //нижняя часть
     it('footer', function () {
-        cy.get('._2MwC')
+        cy.get('._2MwC6')
             .contains('О компании')
             .click();
         cy.get('#root')
             .contains('Мы рады приветствовать вас на сайте нашей компании.')
             .should('be.visible');
-        cy.get('._2MwC')
+        cy.get('._2MwC6')
             .contains('Доставка')
             .click();
         cy.get('#root')
             .contains('AlfaDetali осуществляет доставку Товара:')
             .should('be.visible');
-        cy.get('._2MwC')
+        cy.get('._2MwC6')
             .contains('Как купить')
             .click();
         cy.get('#root')
             .contains('Процедура покупки товара в интернет-магазине')
             .should('be.visible');
-        cy.get('._2MwC')
+        cy.get('._2MwC6')
             .contains('Что с моим заказом?')
             .click();
         cy.get('#root')
             .contains('Что с моим заказом?')
             .should('be.visible');
-        cy.get('._2MwC')
+        cy.get('._2MwC6')
             .contains('Новости')
             .click();
         cy.get('#root')
             .contains('Новости и статьи')
             .should('be.visible');
-        cy.get('._2MwC')
+        cy.get('._2MwC6')
             .contains('Контактная информация')
             .click();
         cy.get('#root')
             .contains('Контакты')
             .should('be.visible');
-        cy.get('._2MwC')
+        cy.get('._2MwC6')
             .contains('Возврат и гарантия')
             .click();
         cy.get('#root')
@@ -189,15 +191,15 @@ describe("магаз", function () {
 
     // переход на мобильную версию и обратно
     it('mobile', function () {
-        cy.get('.Y2nU')
+        cy.get('#root')
             .contains('Мобильная версия')
             .click();
-        cy.get(':nth-child(2) > .scLb > ._1rr6')
+        cy.get(':nth-child(2) > .scLbW > ._1rr64')
             .contains('Страница')
             .should('be.visible');
-        cy.get(':nth-child(2) > .fvTR > label > .Ex8k > ._1iTM')
+        cy.get(':nth-child(2) > .fvTRC > label > .Ex8kQ')
             .click();
-        cy.get('.jgX7 > :nth-child(1)')
+        cy.get('.jgX78 > :nth-child(1)')
             .contains('Полная версия сайта')
             .click();
         cy.get('#root')
@@ -213,10 +215,10 @@ describe("магаз", function () {
         cy.get('#root')
             .contains('Вход')
             .should('be.visible');
-        cy.get(':nth-child(1) > ._2EEn > .aEtU')
+        cy.get(':nth-child(1) > ._2EEno > .aEtUW')
             .clear()
             .type('parole@provider.com');
-        cy.get(':nth-child(2) > ._2EEn > .aEtU')
+        cy.get(':nth-child(2) > ._2EEno > .aEtUW')
             .click()
             .type('1234567');
         cy.get('form > button')
