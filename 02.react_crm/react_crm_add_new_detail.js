3a11a7fd5b80b987d4fdf6f61b183c1cb34eed39
MD <martinsdad@icloud.com>
2019-07-12 17:33:55 +0300

    new react scripts

---
+++ 02.react_crm/react_crm_add_new_detail.js
@@ -0,0 +1,213 @@
+let login = ':nth-child(1) > .form-control'
+let password = ':nth-child(2) > .form-control'
+let enter = 'p > .btn'
+let companies = '.css-11unzgr'
+let storages = '.css-11unzgr'
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max - min)
+  return rand;
+};
+
+let randProvider = randomInteger(0, 49).toFixed(0); // поставщик
+let randGroup = randomInteger(0, 17).toFixed(0); // группа
+let randName = randomInteger(0, 10).toFixed(0); // наименование
+let randProducer = randomInteger(0, 500).toFixed(0); // производитель
+let randColor = randomInteger(0, 7).toFixed(0); // цвет
+let randRate = randomInteger(0, 4).toFixed(0); // рейтинг
+let randDefect = randomInteger(0, 24).toFixed(0); // рейтинг
+let randBrand = randomInteger(0, 88).toFixed(0);
+let randModel = randomInteger(0, 1).toFixed(0);
+let randGen = randomInteger(0, 0).toFixed(0);
+
+describe('add new detail', function () {
+  before('cookie', function () {
+    cy.setCookie('guid-1', '%7B67F7C782-1A21-C70D-DC3F-4C2B6CA27E6B%7D')
+    cy.server();
+    cy.route('GET', 'https://crm.api.apgrup.ru/v1/*')
+      .as('ww');
+  });
+
+  it("visit apgrup", function () {
+    cy.visit("https://react.apgrup.ru/");
+    cy.get(login)
+      .click()
+      .type('ra-nt-office@yandex.ru');
+    cy.get(password)
+      .click()
+      .type('654321');
+    cy.get(enter)
+      .click();
+    cy.wait('@ww');
+  });
+
+  it('pick ruusian village', function () {
+    cy.get('._2Hrbd')
+      .contains('Компания')
+      .next()
+      .click();
+    cy.get(companies) // выпадающий список со складами
+      .contains('АльфаДетали')
+      .click();
+    cy.get('._2Hrbd')
+      .contains('Склад')
+      .next()
+      .click();
+    cy.get(storages) // выпадающий список со складами
+      .contains('РУССКАЯ')
+      .click();
+    cy.wait(3000);
+  });
+
+  it('add new detail', function () {
+    cy.get('._2Hrbd')
+      .contains('Приходные накладные')
+      .click({ force: true });
+    cy.wait(2000);
+    cy.get('.app__content')
+      .contains('Добавить')
+      .click({ force: true });
+    cy.get('.fields > .form__row > .form__label')
+      .next()
+      .click();
+    cy.get('.inputAutocomplete__popup')
+      .eq(2)
+      .children()
+      .eq(randProvider)
+      .click();
+    cy.get('.popup__inner')
+      .contains('Создать')
+      .click();
+    cy.get('.app__content')
+      .contains('Добавить деталь')
+      .click();
+    cy.wait(1000);
+    cy.get(':nth-child(1) > .form__label')
+      .next()
+      .click();
+    cy.get('.inputAutocomplete__popup')
+      .eq(2) // группа
+      .children()
+      .eq(randGroup)
+      .click();
+    cy.get(':nth-child(2) > .form__label')
+      .next()
+      .click();
+    cy.get('.inputAutocomplete__popup')
+      .eq(3) // наименование
+      .children()
+      .eq(randName)
+      .click();
+    cy.get('#form-quantity')
+      .type('1')
+      .should('value', '1');
+    cy.get(':nth-child(11) > .form__label')
+      .next()
+      .click();
+    cy.get('.inputAutocomplete__popup')
+      .eq(5) // производитель
+      .children()
+      .eq(randProducer)
+      .click();
+    cy.get(':nth-child(15) > .form__label')
+      .next()
+      .click();
+    cy.get('.inputAutocomplete__popup')
+      .eq(6) // Цвет
+      .children()
+      .eq(randColor)
+      .click();
+    cy.get(':nth-child(16) > .form__label')
+      .next()
+      .click();
+    cy.get('.inputAutocomplete__popup')
+      .eq(7) // Рейтинг
+      .children()
+      .eq(randRate)
+      .click();
+    cy.get('.modal-body')
+      .contains('Добавить деталь')
+      .click();
+    cy.wait(1000);
+  });
+
+  it('add new car', function () {
+    cy.get('#carPiсker-brand')
+      .click();
+    cy.get('.inputAutocomplete__popup') // список с марками
+      .eq(8)
+      .children()
+      .eq(randBrand)
+      .click();
+    cy.get('#carPiсker-model')
+      .click();
+    cy.get('.inputAutocomplete__popup') // список с моделями
+      .eq(9)
+      .children()
+      .eq(randModel)
+      .click();
+    cy.get('#carPiсker-generation')
+      .click()
+    cy.get('.inputAutocomplete__popup') // список с моделями
+      .eq(10)
+      .children()
+      .eq(randGen)
+      .click();
+    cy.get(':nth-child(2) > .panel-body')
+      .contains('Добавить')
+      .click();
+    // cy.get('tbody > tr.ng-scope > :nth-child(3)') // область для проверки поколения авто на видимость после добавления к детали в окне ред.
+    //   .contains('A3 [8PA] 2004 - 2013')
+    //   .should('be.visible');
+  });
+
+  it('add defeсt', function () {
+    cy.get(':nth-child(4) > .panel-heading')
+      .next()
+      .click();
+    cy.get('.inputAutocomplete__popup')
+      .eq(11) // дефект
+      .children()
+      .eq(randDefect)
+      .click();
+    cy.get(':nth-child(4) > .panel-body')
+      .contains('Добавить')
+      .click();
+    cy.wait(3000);
+    cy.get(':nth-child(4) > .panel-body') // область с названием добавленного дефекта
+      .should('have.attr', 'class');
+    cy.get('.popup__close')
+      .click();
+    cy.wait(2000);
+  });
+
+  it('submit detail', function () {
+    cy.get(':nth-child(2) > .inputBoolean > .inputBoolean__checkbox')
+      .click();
+    cy.get('.app__content')
+      .contains('Подтвердить')
+      .click();
+  });
+
+  it('add price', function () {
+    cy.get('tr > :nth-child(4) > a')
+      .click();
+    cy.wait(2000)
+    cy.get('.app__content')
+      .contains('Оценить')
+      .click();
+    cy.get('#form-purchasePrice')
+      .click()
+      .type('777');
+    cy.get('#form-sellingPrice')
+      .click()
+      .type('777');
+    cy.get('.popup__inner')
+      .contains('Сохранить')
+      .click();
+    cy.wait(2000);
+    cy.get('.partModels__item')
+      .contains('Открыть в магазине')
+      .click({ force: true });
+  });
+});
---
+++ 02.react_crm/react_crm_euroavto_visible.js
@@ -0,0 +1,160 @@
+let login = ':nth-child(1) > .form-control'
+let password = ':nth-child(2) > .form-control'
+let enter = 'p > .btn'
+let companies = '.css-11unzgr'
+let storages = '.css-11unzgr'
+let name = '#form-name'
+let phone = '#form-phoneNumber'
+let para = ':nth-child(15) > :nth-child(2) > a'
+
+describe('Create an order', function () {
+  before('cookie', function () {
+    cy.setCookie('guid-1', '%7B67F7C782-1A21-C70D-DC3F-4C2B6CA27E6B%7D')
+    cy.server();
+    cy.route('GET', 'https://crm.api.apgrup.ru/v1/*')
+      .as('ww');
+  });
+
+  it('visit apgrup', function () {
+    cy.visit("https://react.apgrup.ru/");
+
+    cy.get(login)
+      .click()
+      .clear()
+      .type('ra-nt-office@yandex.ru');
+    cy.get(password)
+      .click()
+      .clear()
+      .type('654321');
+    cy.get(enter)
+      .click();
+    cy.wait('@ww');
+  });
+
+  it('pick ruusian village', function () {
+    cy.get('._2Hrbd')
+      .contains('Компания')
+      .next()
+      .click();
+    cy.get(companies) // выпадающий список со складами
+      .contains('АльфаДетали')
+      .click();
+      cy.get('._2Hrbd')
+      .contains('Склад')
+      .next()
+      .click();
+    cy.get(storages) // выпадающий список со складами
+      .contains('РУССКАЯ')
+      .click();
+    cy.wait(3000);
+  });
+
+  it('preorder', function () {
+    cy.get('._2Hrbd')
+      .contains('Добавить предзаказ')
+      .click({ force: true });
+    cy.get('.app__content')
+      .contains('Создать')
+      .click();
+    cy.get('.row') // уведомление под полем ввода имя
+      .contains('Поле обязательно для')
+      .should('be.visible');
+    cy.get(name) // поле имя
+      .type('Roman')
+      .should('value', 'Roman');
+    cy.get(phone) // поле телефон
+      .type('79992070525')
+      .should('value', '+79992070525');
+    cy.get('.has-error > .form__label') // взять селектор слова "Источник" и некстом взять его поле
+      .next()
+      .click();
+    cy.get('.app__content') // выбрать на станице "Другое" и кликнуть
+      .contains('Другое')
+      .click();
+    cy.get('.app__content')
+      .contains('Создать')
+      .click();
+    cy.wait(2000);
+    cy.get('.messengerUpload')
+      .contains('Предзаказ №')
+      .should('be.visible');
+    cy.get('.messengerUpload')
+      .contains('Roman')
+      .should('be.visible');
+    cy.get('#form-description') // поле сообщение*
+      .click()
+      .type('test');
+  });
+
+  it('Search and add detail', function () {
+    cy.get('#quickSearch')
+      .click();
+    cy.get('.partsQuickSearch__content')
+      .contains('Свернуть')
+      .should('be.visible');
+    cy.get(':nth-child(1) > .row > :nth-child(1) > [field="field"] > .form-control') //поле машина
+      .click();
+    cy.get('.modal-body') // список марок в поле поиска
+      .contains('Audi')
+      .should('be.visible')
+      .click();
+    cy.get('.modal-body')
+      .contains('A8')
+      .should('be.visible')
+      .click();
+    cy.get('.modal-body')
+      .contains('A8 [D4] 2010 - 2017')
+      .should('be.visible')
+      .click();
+    cy.get('[style="margin-bottom: 20px"] > :nth-child(1) > .row > :nth-child(2) > [field="field"] > .form-control') // поле наименование
+      .click({ force: true });
+    cy.get('.modal-body')
+      .contains('Система охлаждения')
+      .click({ force: true });
+    cy.get('.modal-body')
+      .contains('Патрубок')
+      .click({ force: true });
+    cy.get('.pull-right > .btn-success') // кнопка применить во всплывающем поиске
+      .click({ force: true });
+    cy.wait(2000);
+    cy.get('.partsQuickSearch__content')
+      .contains('Добавить')
+      .click({ force: true });
+    cy.get('.draggableWindow__close')
+      .click({ force: true });
+    cy.get('.app__content')
+      .contains('Патрубок')
+      .should('be.visible');
+    cy.get('.messengerUpload')
+      .contains('Создать заказ')
+      .click();
+    cy.get('.modal-footer')
+      .contains('Создать')
+      .click();
+    cy.wait(2000);
+    cy.get('.app__content')
+      .contains('работе')
+      .should('be.visible');
+    cy.get('.app__content')
+      .contains('Отказ')
+      .click();
+    cy.get(':nth-child(1) > .form__label')
+      .next()
+      .click();
+    cy.get('[name=""] > .inputAutocomplete > .inputPopup > .inputPopup__popup')
+      .contains('Дорого')
+      .click();
+    cy.get('.popup__inner')
+      .contains('Сохранить')
+      .click();
+    cy.get('.app__content')
+      .contains('Подтвердить')
+      .click();
+    cy.get('.modal-footer')
+      .contains('Отменить')
+      .click();
+    cy.get('.app__content')
+      .contains('Закрыт')
+      .should('be.visible');
+  });
+});
